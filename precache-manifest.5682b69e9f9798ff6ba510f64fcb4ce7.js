10b569030961bd7d4aba2dae07589bfb29d885dd
yuchuan <yuchuan@yuchuan.com>
2019-07-11 14:22:59 -0400

    Updates

---
+++ precache-manifest.5682b69e9f9798ff6ba510f64fcb4ce7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cce924c1daf8310f2575aed1505e898",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "cc7532f4b24628bab24b",
+    "url": "/monster-rolodex/static/css/main.2a3f1e2b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "cc7532f4b24628bab24b",
+    "url": "/monster-rolodex/static/js/main.966ce99e.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  }
+]);
